07c8b399da5a41e0baf6ce4348f2d03dd2cd1d87
Vishal Jawadwar <vsjawadwar@gmail.com>
2024-07-09 23:32:24 +0530

    Video model has been created successfully

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,43 @@
 import mongoose, {Schema} from "mongoose";
 
-const videoSchema = new Schema({});
+const videoSchema = new Schema(
+    {
+       videoFile: {
+        type: String, //Cloudinary url
+        required: true,
+       },
+       thumbnail: {
+        type: String,
+        required: true
+       },
+       title: {
+        type: String,
+        required: true
+       },
+       description: {
+        type: String,
+        required: true
+       },
+       duration: {
+        type: Number,
+        requied: true
+       },
+       views: {
+        type: Number,
+        default: 0
+       },
+       isPublished: {
+        type: Boolean,
+        default: true
+       },
+       owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+       }
+    },
+    {
+        timestamps: true
+    }
+);
 
 export const Video = mongoose.model("Video", videoSchema);
