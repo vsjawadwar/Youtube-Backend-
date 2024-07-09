f9f16bae1b40ca7c9bc346bdfb5d259d87d14118
Vishal Jawadwar <vsjawadwar@gmail.com>
2024-07-10 00:08:57 +0530

    User model updated with access and refresh token

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
@@ -40,4 +41,5 @@ const videoSchema = new Schema(
     }
 );
 
+videoSchema.plugin(mongooseAggregatePaginate);
 export const Video = mongoose.model("Video", videoSchema);
