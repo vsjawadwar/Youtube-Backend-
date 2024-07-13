3d83c2c0c2a849ebfdea40591506d65c50c9f698
Vishal Jawadwar <vsjawadwar@gmail.com>
2024-07-13 23:17:21 +0530

    Server has been created successfully

--- src/app.js
+++ src/app.js
@@ -15,4 +15,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//Routes
+import userRouter from './routes/user.routes.js';
+
+
+//Routes Declaration
+app.use("/api/v1/users", userRouter);
+//http://localhost:8000/api/v1/users
+
+
 export {app};
